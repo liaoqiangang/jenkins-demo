65e7f6aff0aaf1867827315cf4bcd77bf10e376e
liaoqiangang <liaoqiangang@outlook.com>
2020-04-13 11:01:26 +0800

    springboot 改成war包

--- src/main/java/com/infocity/ProductApp.java
+++ src/main/java/com/infocity/ProductApp.java
@@ -1,9 +1,12 @@
 package com.infocity;
 
+import com.infocity.vo.Product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author liaoqiangang
@@ -15,9 +18,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.infocity.mapper")
 @EnableAutoConfiguration
-public class ProductApp {
+public class ProductApp extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(ProductApp.class);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(ProductApp.class);
   }
 }
